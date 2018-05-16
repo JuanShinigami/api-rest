1131739fc963de5c1698794b0e47b42f4b699fc9
vanessa velasco <vane.velascogtz@gmail.com>
2018-05-16 16:10:00 -0500

    agregar modulo auth

--- config/application.config.php
+++ config/application.config.php
@@ -11,6 +11,7 @@ return array(
         'Application',
         'Supplier',
         'DatosProveedor',
+        'Auth'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
